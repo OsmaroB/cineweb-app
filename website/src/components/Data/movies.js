5ac15541f030c7c41068250a66d640a79271a429
OsmaroB <36721551+OsmaroB@users.noreply.github.com>
2022-06-10 22:02:22 -0600

    finish website standart

---
+++ website/src/components/Data/movies.js
@@ -0,0 +1,36 @@
+const movies = [
+    {
+        "id": "MOV001",
+        "name": "Jurassic World Dominio",
+        "schedule": "4:30PM / 6:30PM / 10:00PM",
+        "synopsis": "Cuatro años después de la destrucción de Isla Nublar, los dinosaurios ahora viven y cazan junto a los humanos en todo el mundo. Este frágil equilibrio remodelará el futuro y determinará, de una vez por todas, si los seres humanos seguirán siendo los depredadores máximos en un planeta que ahora comparten con las criaturas más temibles de la historia.",
+        "cast": "Bryce Dallas Howard,Laura Dern,Jeff Goldblum,Chris Pratt,Sam Neill",
+        "img": "https://firebasestorage.googleapis.com/v0/b/cineweb-app.appspot.com/o/img%2Fjurassic_world.jpg?alt=media&token=7a2683f5-faeb-4e3d-ad59-d47351e0cc15"
+    },
+    {
+        "id": "MOV002",
+        "name": "Top Gun: Maverick",
+        "schedule": "4:30PM / 6:30PM / 10:00PM",
+        "synopsis": "Después de más de 30 años de servicio como uno de los mejores aviadores de la Armada, Pete 'Mavericks' Mitchel (Tom Cruise) se encuentra dónde siempre quiso estar, empujando los límites como un valiente piloto de prueba y esquivando el alcance en su rango, que no le dejaría volar emplazándolo en tierra. Cuando se encuentra entrenando a un destacamento de graduados de Top Gun para una misión especializada, Maverick se encuentra allí con el teniente Bradley Bradshaw (Miles Teller), el hijo de su difunto amigo 'Goose'... Secuela de Top Gun.",
+        "cast": "Tom Cruise,Miles Teller,Val Kilmer",
+        "img": "https://firebasestorage.googleapis.com/v0/b/cineweb-app.appspot.com/o/img%2Ftop_gun.jpg?alt=media&token=c69356b8-3416-4f52-8430-efbbbe8a1c0f"
+    },
+    {
+        "id": "MOV003",
+        "name": "Asesino sin memoria",
+        "schedule": "4:30PM / 6:30PM / 10:00PM",
+        "synopsis": "Un asesino a sueldo descubre que se ha convertido en un objetivo después de que se niega a completar un trabajo para una peligrosa organización criminal... Remake de la película belga de 2003 'The Memory of a Killer'.",
+        "cast": "Liam Neeson,Ray Stevenson,Monica Bellucci,Guy Pearce",
+        "img": "https://firebasestorage.googleapis.com/v0/b/cineweb-app.appspot.com/o/img%2Fasesino_sin_memoria.jpg?alt=media&token=c18e6a77-95c6-48f9-b264-342a656aa39b"
+    },
+    {
+        "id": "MOV004",
+        "name": "Gemelo siniestro",
+        "schedule": "4:30PM / 6:30PM / 10:00PM",
+        "synopsis": "Una madre necesita enfrentarse a la insoportable verdad sobre su hijo gemelo sobreviviente.",
+        "cast": "Teresa Palmer,Steven Cree,Barbara Marten,Tristan Ruggeri",
+        "img": "https://firebasestorage.googleapis.com/v0/b/cineweb-app.appspot.com/o/img%2Fgemelo_siniestro.jpg?alt=media&token=edebe392-e969-4b05-bbe1-4a7309d79d9d"
+    }
+]
+
+export default movies;
--- website/src/pages/details/[id].js
+++ website/src/pages/details/[id].js
@@ -1,37 +1,55 @@
-import * as React from "react"
+import React, {useEffect, useState} from "react"
 
 import Layout from "../../components/layout"
 import Seo from "../../components/seo"
+import movies from "../../components/Data/movies"
 
-const Detalle = ({id}) => (
-  <Layout>
-    <Seo title="Detalle de peliculas" />
-    <div className="container">
-        <div className="row mt-5 mb-5">
-            <div className="col-12 col-sm-12 col-md-8 col-lg-8">
-                <h1>Titulo de la pelicula {id}</h1>
-            </div>
-            <div className="col-12 col-sm-12 col-md-4 col-lg-4">
-                <img src="" alt="Imagen de la pelicula" />
-            </div>
-        </div>
-        <div className="row mt-3 mb-3">
-            <div className="col-12 col-sm-12 col-md-6 col-lg-6">
-                <p><b>Horarios: </b> Lunes a viernes de 6:00PM A 8:00PM</p>
-            </div>
-            <div className="col-12 col-sm-12 col-md-6 col-lg-6">
-                <p><b>Protagonistas: </b> Reparto general - es el reparto</p>
-            </div>
-        </div>
-        <div className="row mt-3 mb-3">
-            <div className="col-12 col-sm-12 col-md-12 col-lg-12">
-                <p className="text-left"><b>Sinopsis:</b>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Alias, aperiam rerum? Accusamus illum atque praesentium. Nam neque facere aspernatur pariatur, accusantium possimus tempora vero eaque, enim beatae veritatis quis aperiam!
-                Fugit ad impedit debitis assumenda voluptates soluta praesentium fugiat quo magni, facere id a cupiditate sed dolorem, obcaecati molestiae officiis quisquam similique ipsa at esse sunt. Praesentium omnis possimus odio?
-                Magnam sint nisi omnis, vero consequatur aut nobis in minus illo beatae non debitis quia reiciendis corrupti esse accusamus delectus suscipit, voluptate explicabo. Recusandae temporibus praesentium cupiditate! Minus, ut aspernatur?</p>
-            </div>
-        </div>
-    </div>
-  </Layout>
-)
+const Detalle = ({id}) => {
+    const [movieList, setmovie] = useState([]);
+    const readData = () => {
+        const movie = movies.filter(movie => movie.id == id);
+        setmovie(movie);
+    }
+
+
+    useEffect(() => {
+        readData();
+    }, [])
+    
+    return(
+        <Layout>
+            <Seo title="Detalle de peliculas" />
+                {
+                    movieList.map((item)=>{
+                        return(
+                            <div className="container" key={item.id}>
+                                <div className="row mt-5 mb-5">
+                                    <div className="col-12 col-sm-12 col-md-8 col-lg-8">
+                                        <h1>{item.name}</h1>
+                                    </div>
+                                    <div className="col-12 col-sm-12 col-md-4 col-lg-4">
+                                        <img src={item.img} className="rounded shadow" alt="Imagen de la pelicula" />
+                                    </div>
+                                </div>
+                                <div className="row mt-3 mb-3">
+                                    <div className="col-12 col-sm-12 col-md-6 col-lg-6">
+                                        <p><b>Horarios: </b>{item.schedule}</p>
+                                    </div>
+                                    <div className="col-12 col-sm-12 col-md-6 col-lg-6">
+                                        <p><b>Protagonistas: </b> {item.cast}</p>
+                                    </div>
+                                </div>
+                                <div className="row mt-3 mb-3">
+                                    <div className="col-12 col-sm-12 col-md-12 col-lg-12">
+                                        <p className="text-left"><b>Sinopsis: </b>{item.synopsis}</p>
+                                    </div>
+                                </div>
+                            </div>
+                        )
+                    })
+                }
+        </Layout>
+    );
+}
 
 export default Detalle
